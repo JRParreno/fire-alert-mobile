70c55ad258d3288f7ef4dcd84a1def6f9d5e41c6
JRParreno <jhonrhayparreno22@gmail.com>
2023-04-22 19:27:30 +0800

    folder setup

---
+++ lib/src/core/routes/app_route.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+// Route<dynamic>? generateRoute(RouteSettings settings) {
+//   return MaterialPageRoute(builder: (context) {
+//     switch (settings.name) {
+//       case Screen.routeName:
+//         final args = settings.arguments! as ScreenArgs;
+//         return Screen(args: args);
+//     }
+//     return const Scaffold(
+//       resizeToAvoidBottomInset: false,
+//       backgroundColor: Assets.,
+//       body: Center(
+//         child: Text('no screen'),
+//       ),
+//     );
+//   });
+// }
