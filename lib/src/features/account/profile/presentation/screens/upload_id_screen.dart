20223786f3005882a5db3b8561fbda4a586d123b
JRParreno <jhonrhayparreno22@gmail.com>
2023-11-12 22:18:30 +0800

    added info in upload id screen

--- lib/src/features/account/profile/presentation/screens/upload_id_screen.dart
+++ lib/src/features/account/profile/presentation/screens/upload_id_screen.dart
@@ -1,7 +1,9 @@
+import 'package:fire_alert_mobile/gen/assets.gen.dart';
 import 'package:fire_alert_mobile/src/core/bloc/common/common_event.dart';
 import 'package:fire_alert_mobile/src/core/bloc/profile/profile_bloc.dart';
 import 'package:fire_alert_mobile/src/core/common_widget/common_widget.dart';
 import 'package:fire_alert_mobile/src/core/common_widget/custom_appbar.dart';
+import 'package:fire_alert_mobile/src/core/common_widget/v_space.dart';
 import 'package:fire_alert_mobile/src/core/config/app_constant.dart';
 import 'package:fire_alert_mobile/src/core/utils/profile_utils.dart';
 import 'package:fire_alert_mobile/src/features/account/profile/data/repositories/profile_repository_impl.dart';
@@ -25,6 +27,160 @@ class UploadIDScreen extends StatefulWidget {
 }
 
 class _UploadIDScreenState extends State<UploadIDScreen> {
+  final infoText =
+      "Note: Please submit a picture of you holding your Identification Card front and back for fast verification by the Admin. \n\nThe following ID's will be accepted only:\n\n✅ Student ID\n✅ National ID\n✅ Driver's License\n✅ Postal ID\n✅ Passport ID\n✅ SSS ID";
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        BlocProvider.of<UploadIdBloc>(context).add(const InitialEvent());
+        return true;
+      },
+      child: Scaffold(
+        appBar: buildAppBar(
+            context: context,
+            title: "Verify Account",
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back,
+                size: 25,
+              ),
+              onPressed: () {
+                BlocProvider.of<UploadIdBloc>(context)
+                    .add(const InitialEvent());
+                Navigator.of(context).pop(false);
+              },
+            )),
+        body: SingleChildScrollView(
+          child: BlocBuilder<UploadIdBloc, UploadIdState>(
+            builder: (context, state) {
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Column(
+                      children: [
+                        buildCardBtn(
+                          btnTitle: 'Take Photo',
+                          cardTitle: 'Front Photo ID',
+                          previewPhoto: (imagePath) {
+                            handlePreviewPhoto(
+                              context: context,
+                              imagePath: imagePath,
+                            );
+                          },
+                          takePhoto: () {
+                            handleTakePhoto(context: context);
+                          },
+                          state: state,
+                          viewTitle: "View Front Photo ID",
+                        ),
+                        buildCardBtn(
+                          btnTitle: 'Take Photo',
+                          cardTitle: 'Back Photo ID',
+                          previewPhoto: (imagePath) {
+                            handlePreviewPhoto(
+                              context: context,
+                              imagePath: imagePath,
+                              isFrontPhoto: false,
+                            );
+                          },
+                          takePhoto: () {
+                            handleTakePhoto(
+                                context: context, isFrontPhoto: false);
+                          },
+                          state: state,
+                          viewTitle: "View Back Photo ID",
+                          isFrontPhoto: false,
+                        ),
+                      ],
+                    ),
+                    Vspace.md,
+                    CustomText(text: infoText),
+                    Vspace.md,
+                    Assets.images.infoCard.image(),
+                    Vspace.md,
+                    CustomBtn(
+                      label: "Submit",
+                      onTap: state is UploadIdLoaded &&
+                              state.backPhotoPath != null &&
+                              state.frontPhotoPath != null
+                          ? () {
+                              handleUploadId(state);
+                            }
+                          : null,
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildCardBtn({
+    required UploadIdState state,
+    required String btnTitle,
+    required String viewTitle,
+    required VoidCallback takePhoto,
+    required Function(String) previewPhoto,
+    required String cardTitle,
+    bool isFrontPhoto = true,
+  }) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(
+              text: cardTitle,
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            if (state is UploadIdLoaded) ...[
+              if (isFrontPhoto && state.frontPhotoPath != null) ...[
+                CustomBtn(
+                  label: viewTitle,
+                  onTap: () {
+                    previewPhoto(state.frontPhotoPath!);
+                  },
+                  backgroundColor: Colors.blue,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const Divider(
+                  height: 20,
+                ),
+              ],
+              if (!isFrontPhoto && state.backPhotoPath != null) ...[
+                CustomBtn(
+                  label: viewTitle,
+                  onTap: () {
+                    previewPhoto(state.backPhotoPath!);
+                  },
+                  backgroundColor: Colors.blue,
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const Divider(
+                  height: 20,
+                ),
+              ],
+            ],
+            CustomBtn(
+              label: btnTitle,
+              onTap: takePhoto,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   void handleTakePhoto({
     bool isFrontPhoto = true,
     required BuildContext context,
@@ -119,153 +275,5 @@ class _UploadIDScreenState extends State<UploadIDScreen> {
         ],
       ).show(context);
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        BlocProvider.of<UploadIdBloc>(context).add(const InitialEvent());
-        return true;
-      },
-      child: Scaffold(
-        appBar: buildAppBar(
-            context: context,
-            title: "Verify Account",
-            leading: IconButton(
-              icon: const Icon(
-                Icons.arrow_back,
-                size: 25,
-              ),
-              onPressed: () {
-                BlocProvider.of<UploadIdBloc>(context)
-                    .add(const InitialEvent());
-                Navigator.of(context).pop(false);
-              },
-            )),
-        body: BlocBuilder<UploadIdBloc, UploadIdState>(
-          builder: (context, state) {
-            return Container(
-                height: double.infinity,
-                width: double.infinity,
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Flexible(
-                      flex: 1,
-                      child: Column(
-                        children: [
-                          buildCardBtn(
-                            btnTitle: 'Take Photo',
-                            cardTitle: 'Front Photo ID',
-                            previewPhoto: (imagePath) {
-                              handlePreviewPhoto(
-                                context: context,
-                                imagePath: imagePath,
-                              );
-                            },
-                            takePhoto: () {
-                              handleTakePhoto(context: context);
-                            },
-                            state: state,
-                            viewTitle: "View Front Photo ID",
-                          ),
-                          buildCardBtn(
-                            btnTitle: 'Take Photo',
-                            cardTitle: 'Back Photo ID',
-                            previewPhoto: (imagePath) {
-                              handlePreviewPhoto(
-                                context: context,
-                                imagePath: imagePath,
-                                isFrontPhoto: false,
-                              );
-                            },
-                            takePhoto: () {
-                              handleTakePhoto(
-                                  context: context, isFrontPhoto: false);
-                            },
-                            state: state,
-                            viewTitle: "View Back Photo ID",
-                            isFrontPhoto: false,
-                          ),
-                        ],
-                      ),
-                    ),
-                    CustomBtn(
-                      label: "Submit",
-                      onTap: state is UploadIdLoaded &&
-                              state.backPhotoPath != null &&
-                              state.frontPhotoPath != null
-                          ? () {
-                              handleUploadId(state);
-                            }
-                          : null,
-                    )
-                  ],
-                ));
-          },
-        ),
-      ),
-    );
-  }
-
-  Widget buildCardBtn({
-    required UploadIdState state,
-    required String btnTitle,
-    required String viewTitle,
-    required VoidCallback takePhoto,
-    required Function(String) previewPhoto,
-    required String cardTitle,
-    bool isFrontPhoto = true,
-  }) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            CustomText(
-              text: cardTitle,
-              style: const TextStyle(fontSize: 18),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            if (state is UploadIdLoaded) ...[
-              if (isFrontPhoto && state.frontPhotoPath != null) ...[
-                CustomBtn(
-                  label: viewTitle,
-                  onTap: () {
-                    previewPhoto(state.frontPhotoPath!);
-                  },
-                  backgroundColor: Colors.blue,
-                  style: const TextStyle(color: Colors.white),
-                ),
-                const Divider(
-                  height: 20,
-                ),
-              ],
-              if (!isFrontPhoto && state.backPhotoPath != null) ...[
-                CustomBtn(
-                  label: viewTitle,
-                  onTap: () {
-                    previewPhoto(state.backPhotoPath!);
-                  },
-                  backgroundColor: Colors.blue,
-                  style: const TextStyle(color: Colors.white),
-                ),
-                const Divider(
-                  height: 20,
-                ),
-              ],
-            ],
-            CustomBtn(
-              label: btnTitle,
-              onTap: takePhoto,
-            )
-          ],
-        ),
-      ),
-    );
   }
 }
